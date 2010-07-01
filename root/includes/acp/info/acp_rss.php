e3061a4835140584b6a2ef33fb9316c4296cfa67
Gabriel <forum@mssti.com>
2010-07-01 02:01:56 -0300

    Committed Version 1.0.3c

---
+++ root/includes/acp/info/acp_rss.php
@@ -0,0 +1,37 @@
+<?php
+/**
+* @package: phpBB3 :: RSS feed 2.0 -> includes -> acp -> info
+* @version: $Id: acp_rss.php, v 1.0.3 2008/07/23 23:07:08 leviatan21 Exp $
+* @copyright: leviatan21 < info@mssti.com > (Gabriel) http://www.mssti.com/phpbb3/
+* @license: http://opensource.org/licenses/gpl-license.php GNU Public License
+* @author: leviatan21 - http://www.phpbb.com/community/memberlist.php?mode=viewprofile&u=345763
+*
+**/
+
+/**
+* @package module_install
+*/
+class acp_rss_info
+{
+	function module()
+	{
+		return array(
+			'filename'	=> 'acp_rss',
+			'title'		=> 'ACP_RSS',
+			'version'	=> '1.0.0',
+			'modes'		=> array(
+				'rss_feeds'	=> array('title' => 'ACP_RSS_FEEDS', 'auth' => 'acl_a_board', 'cat' => array('ACP_GENERAL_TASKS')),
+			),
+		);
+	}
+
+	function install()
+	{
+	}
+
+	function uninstall()
+	{
+	}
+}
+
+?>
